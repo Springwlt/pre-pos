c16d805747d1cb29a85b08a130cc4815e6fabeaf
123 <wanglitao1202@outlook.com>
2015-05-17 19:53:14 +0800

    提交5,6

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var  common= [];
+for (var  i = 0;  i < collection_a.length; i ++){
+    for(var j = 0; j < 4; j++){
+           if(collection_a[i].key ==object_b.value[j]){
+                 common.push(collection_a[i].key);
+           }
+    }
+}
+       return common;
 }
 
 module.exports = collect_same_elements;
--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,83 @@
 function count_same_elements(collection) {
-  //在这里写入代码
-}
+    var a_commom=[];
+    var e_commom=[];
+    var h_commom=[];
+    var t_commom=[];
+    var f_commom=[];
+    var c_commom=[];
+    var g_commom=[];
+    var b_commom=[];
+    var d_commom=[];
+    var array=[];
+    var obj = {};
+    var obja = {},obje = {},objh={},objt={},objf={},objc={},objg={},objb={},objd={};
+    for(var i = 0; i < collection.length; i++){
+        if(collection[i]=='a'){
+                 a_commom.push(collection[i]);
+        }
+        else if(collection[i]=='e'){
+                   e_commom.push(collection[i]);
+        }
+        else if(collection[i]=='h'){
+                  h_commom.push(collection[i]);
+        }
+        else if(collection[i]=='t'){
+                  t_commom.push(collection[i]);
+        }
+        else if(collection[i]=='f'){
+                    f_commom.push(collection[i]);
+        }
+        else if(collection[i]=='c'){
+                   c_commom.push(collection[i]);
+        }
+        else if(collection[i]=='g'){
+                   g_commom.push(collection[i]);
+        }
+        else if(collection[i]=='b'){
+                     b_commom.push(collection[i]);
+        }
+        else if(collection[i]=='d'){
+                  d_commom.push(collection[i]);
+        }
+    }
+    obja.key='a';
+    obja.count=a_commom.length;
+    array.push(obja);
 
-module.exports = count_same_elements;
+    obje.key='e';
+    obje.count=e_commom.length;
+    array.push(obje);
+
+    objh.key='h';
+    objh.count=h_commom.length;
+    array.push(objh);
+
+    objt.key='t';
+    objt.count=t_commom.length;
+    array.push(objt);
+
+    objf.key='f';
+    objf.count=f_commom.length;
+    array.push(objf);
+
+    objc.key='c';
+    objc.count=c_commom.length;
+    array.push(objc);
+
+    objg.key='g';
+    objg.count=g_commom.length;
+    array.push(objg);
+
+    objb.key='b';
+    objb.count=b_commom.length;
+    array.push(objb);
+
+    objd.key='d';
+    objd.count=d_commom.length;
+    array.push(objd);
+
+
+
+    return array;
+}
+module.exports = count_same_elements
